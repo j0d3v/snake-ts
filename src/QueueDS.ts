1289b9a92636f4cb572220c215d1e88a8e8dcc3b
Youssef Eddaif <183735203+j0d3v@users.noreply.github.com>
2025-04-01 22:14:56 +0000

    Implement PositionQueue for managing snake positions
    Signed-off-by: Youssef Eddaif <183735203+j0d3v@users.noreply.github.com>

---
+++ src/QueueDS.ts
@@ -0,0 +1,33 @@
+export type Position = {
+  x: number;
+  y: number;
+};
+interface Queue<T> {
+  items: Array<T>;
+  capacity: number;
+  size(): number;
+  enqueue(item: T): void;
+  dequeue(): T | undefined;
+  isEmpty(): boolean;
+}
+export class PositionQueue implements Queue<Position> {
+  items: Position[] = [];
+  capacity: number;
+
+  constructor(cap: number) {
+    this.capacity = cap;
+  }
+  enqueue(item: Position): void {
+    if (this.size() < this.capacity) this.items.push(item);
+    else console.error("The stack capacity has been exceeded.");
+  }
+  dequeue(): Position | undefined {
+    return this.items.shift();
+  }
+  size(): number {
+    return this.items.length;
+  }
+  isEmpty(): boolean {
+    return this.items.length == 0;
+  }
+}
