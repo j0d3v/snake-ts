41eeeeb77930d24ff54ca4e63e9c80ffa26b9b1e
Youssef Eddaif <183735203+j0d3v@users.noreply.github.com>
2025-04-02 21:58:06 +0000

    Implement snake length growth with Queue data structure - Introduced Queue to manage snake body segments - Snake length increases when food is eaten, and tail is properly removed - Queue ensures correct body position tracking and movement
    Signed-off-by: Youssef Eddaif <183735203+j0d3v@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import {
   GRID_SIZE,
   randomPosition,
 } from "./boardUtils";
+import { PositionQueue } from "./QueueDS";
 
 const board = document.getElementById("board") as HTMLCanvasElement;
 board.width = BOARD_SIZE;
@@ -24,19 +25,28 @@ const directions: Map<string, number[]> = new Map([
   ["ArrowLeft", [-1, 0]],
   ["ArrowRight", [1, 0]],
 ]);
-
+const QMovements = new PositionQueue(GRID_SIZE);
+let snakeLength = 1;
 fillCell(headPos, ctx);
 window.addEventListener("keydown", (event) => {
   event.preventDefault();
-  clearCell(headPos, ctx);
+  let prevPos = { ...headPos };
+  if (QMovements.size() >= snakeLength) {
+    let tail = QMovements.dequeue();
+    if (tail) clearCell(tail, ctx);
+  }
   if (directions.has(event.code)) {
     const [dx, dy] = directions.get(event.code)!;
-    headPos.x += headPos.x + dx > 0 && headPos.x + dx < GRID_SIZE - 1 ? dx : 0;
-    headPos.y += headPos.y + dy > 0 && headPos.y + dy < GRID_SIZE - 1 ? dy : 0;
+    headPos.x +=
+      headPos.x + dx >= 0 && headPos.x + dx <= GRID_SIZE - 1 ? dx : 0;
+    headPos.y +=
+      headPos.y + dy >= 0 && headPos.y + dy <= GRID_SIZE - 1 ? dy : 0;
+    QMovements.enqueue(prevPos);
   }
   if (headPos.x == foodPos.x && headPos.y == foodPos.y) {
     foodPos = randomPosition();
     drawImage(food, foodPos, ctx);
+    snakeLength++;
   }
   fillCell(headPos, ctx);
 });
