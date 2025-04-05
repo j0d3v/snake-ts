abd1441b8162aaced325f8d3b0fe4cdaa0343ec7
Youssef Eddaif <183735203+j0d3v@users.noreply.github.com>
2025-04-05 15:36:37 +0000

    Optimize game loop and implement game over
    Signed-off-by: Youssef Eddaif <183735203+j0d3v@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ const board = document.getElementById("board") as HTMLCanvasElement;
 board.width = BOARD_SIZE;
 board.height = BOARD_SIZE;
 const ctx = board.getContext("2d")!;
+const FOOD_IMAGE_SRC = "apple.svg";
 
 const directions: Map<string, Direction> = new Map([
   ["ArrowUp", Direction.Up],
@@ -14,8 +15,16 @@ const directions: Map<string, Direction> = new Map([
   ["ArrowRight", Direction.Right],
 ]);
 
-const snake = new Snake(ctx);
-const food = new Food(randomPosition(), "apple.svg", ctx);
+let food: Food;
+let snake: Snake;
+
+const initGame = () => {
+  food = new Food(randomPosition(), FOOD_IMAGE_SRC, ctx);
+  snake = new Snake(ctx);
+  drawBoard(ctx);
+  snake.draw();
+  food.draw();
+};
 
 window.addEventListener("keydown", (event) => {
   event.preventDefault();
@@ -23,7 +32,10 @@ window.addEventListener("keydown", (event) => {
 
   const direction = directions.get(event.code)!;
 
-  if (!snake.move(direction)) console.log("Snake is couldn't move");
+  if (!snake.move(direction)) {
+    alert("Game over !");
+    initGame();
+  }
 
   // Check if food is eaten
   if (food.collidesWith(snake.currentHead)) {
@@ -36,4 +48,4 @@ window.addEventListener("keydown", (event) => {
   }
 });
 
-drawBoard(ctx);
+initGame();
